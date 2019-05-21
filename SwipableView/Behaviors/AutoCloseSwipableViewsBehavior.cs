f3ad69a398e7612d167eb4bd9bb48c0a8203c20b
Sylvain <sylvain.moingeon@outlook.fr>
2019-05-21 16:14:20 +0200

    Add Auto-close behavior

---
+++ SwipableView/Behaviors/AutoCloseSwipableViewsBehavior.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace SmoDev.Swipable.Behaviors
+{
+    /// <summary>
+    /// Behavior to use with <see cref="SwipableView"/>. 
+    /// This behavior auto-close opened <see cref="SwipableView"/> inside a Layout when you open a <see cref="SwipableView"/>.
+    /// </summary>
+    public class AutoCloseSwipableViewsBehavior : BehaviorBase<Layout<View>>
+    {
+        protected override void OnAttachedTo(Layout<View> bindable)
+        {
+            base.OnAttachedTo(bindable);
+
+            // A ce niveau, les enfants du Layout ne sont pas encore instanciés. 
+            // On s'abonne donc à ChildAdded et ChildRemove pour être averti de l'ajout et suppression d'enfant dans le Layout.
+            bindable.ChildAdded += Bindable_ChildAdded;
+            bindable.ChildRemoved += Bindable_ChildRemoved;
+        }
+
+        private void Bindable_ChildAdded(object sender, ElementEventArgs e)
+        {
+            if (e.Element is SwipableView swipableView)
+            {
+                swipableView.Opening += SwipableView_Opening;
+            }
+        }
+
+        private void SwipableView_Opening(object sender, EventArgs e)
+        {
+            AssociatedObject.Children
+                .Where(child => child is SwipableView swipableView && swipableView != sender && !swipableView.IsClosed)?
+                .Cast<SwipableView>()
+                .ForEach(async swipableView => await swipableView.ClosePanel());
+        }
+
+        private void Bindable_ChildRemoved(object sender, ElementEventArgs e)
+        {
+            if (e.Element is SwipableView swipableView)
+            {
+                swipableView.Closing -= SwipableView_Opening;
+            }
+        }
+
+        protected override void OnDetachingFrom(Layout<View> bindable)
+        {
+            base.OnDetachingFrom(bindable);
+
+            bindable.Children
+                    .Where(child => child is SwipableView)?
+                    .Cast<SwipableView>()
+                    .ForEach(expandableView => expandableView.Closing -= SwipableView_Opening);
+        }
+    }
+}
---
+++ SwipableView/Behaviors/BehaviorBase.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SmoDev.Swipable.Behaviors
+{
+    /// <summary>
+    /// Classe de base pour les <see cref="Behavior{T}"/>.
+    /// La classe expose en particulier une propriété <see cref="AssociatedObject"/> qui donne facilement accès à l'objet sur lequel est attaché le <see cref="Behavior"/>
+    /// </summary>
+    /// <typeparam name="T">Type de l'élément d'IHM sur lequel on attache le <see cref="Behavior"/></typeparam>
+    public class BehaviorBase<T> : Behavior<T> where T : BindableObject
+    {
+        public T AssociatedObject { get; private set; }
+
+        protected override void OnAttachedTo(T bindable)
+        {
+            base.OnAttachedTo(bindable);
+            AssociatedObject = bindable;
+
+            if (bindable.BindingContext != null)
+            {
+                BindingContext = bindable.BindingContext;
+            }
+
+            bindable.BindingContextChanged += OnBindingContextChanged;
+        }
+
+        protected override void OnDetachingFrom(T bindable)
+        {
+            base.OnDetachingFrom(bindable);
+            bindable.BindingContextChanged -= OnBindingContextChanged;
+            AssociatedObject = null;
+        }
+
+        void OnBindingContextChanged(object sender, EventArgs e)
+        {
+            OnBindingContextChanged();
+        }
+
+        protected override void OnBindingContextChanged()
+        {
+            base.OnBindingContextChanged();
+            BindingContext = AssociatedObject.BindingContext;
+        }
+    }
+}
