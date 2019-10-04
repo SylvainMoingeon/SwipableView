39dd3fef2a2a931eb095431c8122674b3543ce41
Sylvain <sylvain.moingeon@outlook.fr>
2019-10-04 16:15:20 +0200

    Change bindableLayout to CollectionView

--- SwipableSample/SwipableSample.Android/MainActivity.cs
+++ SwipableSample/SwipableSample.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
 
 namespace SwipableSample.Droid
 {
@@ -17,6 +18,7 @@ namespace SwipableSample.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+            Forms.SetFlags("CollectionView_Experimental");
             base.OnCreate(savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
